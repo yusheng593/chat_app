0a2a9fd7405527a2d5ade6b025da35b2c6ae2445
Samuel <yusheng593@gmail.com>
2024-10-07 22:38:48 +0800

    firebase 使用信箱可以成功註冊

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:chat_app/screens/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
@@ -11,12 +17,13 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'FlutterChat',
-        theme: ThemeData().copyWith(
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color(0xFF001F3F),
-          ),
+      title: 'FlutterChat',
+      theme: ThemeData().copyWith(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF001F3F),
         ),
-        home: const AuthScreen());
+      ),
+      home: const AuthScreen(),
+    );
   }
 }
--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final _firebase = FirebaseAuth.instance;
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -8,7 +11,42 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _from = GlobalKey<FormState>();
   bool _hasAccount = true;
+
+  String _enteredEmail = '';
+  String _enteredPassword = '';
+
+  void _submit() async {
+    final formState = _from.currentState;
+    if (formState != null && formState.validate()) {
+      formState.save();
+
+      if (_hasAccount) {
+        // log users in
+      } else {
+        try {
+          final userCredentials =
+              await _firebase.createUserWithEmailAndPassword(
+                  email: _enteredEmail, password: _enteredPassword);
+          print(userCredentials);
+        } on FirebaseAuthException catch (error) {
+          if (!mounted) return; // 檢查組件是否仍掛載
+          ScaffoldMessenger.of(context).clearSnackBars();
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(error.message ?? '註冊失敗'),
+            ),
+          );
+        }
+      }
+    } else {
+      return;
+    }
+
+    print('Email: $_enteredEmail, Password: $_enteredPassword');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,24 +70,76 @@ class _AuthScreenState extends State<AuthScreen> {
                     child: Column(
                       children: [
                         Form(
-                          child: TextFormField(
-                            decoration: const InputDecoration(labelText: '信箱'),
-                            keyboardType: TextInputType.emailAddress,
-                            autocorrect: false,
-                            textCapitalization: TextCapitalization.none,
-                          ),
-                        ),
-                        Form(
-                          child: TextFormField(
-                            decoration: const InputDecoration(labelText: '密碼'),
-                            obscureText: true,
+                          key: _from,
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                  labelText: '信箱',
+                                  hintText: '請輸入您的信箱',
+                                  hintStyle: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 14,
+                                    fontStyle: FontStyle.italic,
+                                  ),
+                                ),
+                                keyboardType: TextInputType.emailAddress,
+                                autocorrect: false,
+                                textCapitalization: TextCapitalization.none,
+                                validator: (value) {
+                                  if (value == null || value.trim().isEmpty) {
+                                    return '信箱不能空白';
+                                  }
+                                  final emailRegex = RegExp(
+                                      r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+                                  if (!emailRegex.hasMatch(value.trim())) {
+                                    return '請輸入有效的信箱';
+                                  }
+                                  return null;
+                                },
+                                onSaved: (newValue) {
+                                  if (newValue != null) {
+                                    _enteredEmail = newValue;
+                                  }
+                                },
+                              ),
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                  labelText: '密碼',
+                                  hintText: '最多六位數',
+                                  hintStyle: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 14,
+                                    fontStyle: FontStyle.italic,
+                                  ),
+                                ),
+                                obscureText: true,
+                                validator: (value) {
+                                  if (value == null || value.trim().isEmpty) {
+                                    return '密碼不能空白';
+                                  }
+                                  if (value.contains(' ')) {
+                                    return '密碼不能包含空白鍵';
+                                  }
+                                  if (value.length > 6) {
+                                    return '密碼長度必須在1到6個字元之間';
+                                  }
+                                  return null;
+                                },
+                                onSaved: (newValue) {
+                                  if (newValue != null) {
+                                    _enteredPassword = newValue;
+                                  }
+                                },
+                              ),
+                            ],
                           ),
                         ),
                         const SizedBox(
                           height: 12,
                         ),
                         ElevatedButton(
-                            onPressed: () {},
+                            onPressed: _submit,
                             child: Text(_hasAccount ? '登入' : '註冊')),
                         TextButton(
                             onPressed: () {
